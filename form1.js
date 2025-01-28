576e2f3c75d870e66dd21e6d1b540fa9f9c022d8
dtcpassdelhiigov <rohitmt3608@gmail.com>
2025-01-28 21:26:26 +0530

    Update form1.js

--- form1.js
+++ form1.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
          
         // Check if the mobile number field is wrong
-        if (mobileNumberInput.value !== "8595818420") {
+        if (mobileNumberInput.value !== "8750619544") {
             alert("mobile no. not registered With this passid .\nPlease try again");
             location.reload();
             return; // Stop the function execution here
